e005e668f036384b136faa95c5dc01ab0cb4257b
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 13:10:47 +0300

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab1ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex8
+            Scrieti un program care interschimba valoarea a doua variabile intregi. */
+
+            int x;
+            int y;
+
+            Console.WriteLine("Enter x: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y: ");
+            y = int.Parse(Console.ReadLine());
+
+            //variabiala auxiliar z
+            int z = x;
+            x = y;
+            y = z;
+
+            Console.WriteLine(x + ","+ y);
+            
+        }
+    }
+}
